734c5f791f91a979add6d4dd943e1497745b1da4
Patel <krushan1795@gmail.com>
2021-06-14 16:53:18 -0400

    Adding numbers functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Timer functionality
 let timerEl = document.getElementById('running-timer');
 console.log(timerEl)
 let timerVal = 0;
@@ -6,9 +8,20 @@ setInterval(() => {
     timerEl.innerHTML = timerVal; 
 }, 1000)
 
+//Counter functionality
 let counterBtn = document.getElementById('counter-btn');
 let counter = 0;
 counterBtn.addEventListener("click", () => {
     counter++;
     document.getElementById("counter-num").innerHTML = counter;
   });
+
+//Add numbers functionality
+let submitBtn = document.getElementById('calculate-num-btn');
+submitBtn.addEventListener('click', () => {
+    let numberOne = Number(document.getElementById('number-one').value);
+let numberTwo = Number(document.getElementById('number-two').value);
+
+    document.getElementById('total-text').innerHTML = numberOne + numberTwo
+})
+
